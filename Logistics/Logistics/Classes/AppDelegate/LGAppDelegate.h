b94811a3520fe8dbfe7d7d5b3faf9f10b435b3f0
Diana <d.evlakhova@sealpoint.co>
2013-12-15 22:13:40 +0200

    initial commit

---
+++ Logistics/Logistics/Classes/AppDelegate/LGAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  LGAppDelegate.h
+//  Logistics
+//
+//  Created by Diana on 15.12.13.
+//  Copyright (c) 2013 Diana Evlakhova. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LGAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Logistics/Logistics/Classes/Other/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Logistics
+//
+//  Created by Diana on 15.12.13.
+//  Copyright (c) 2013 Diana Evlakhova. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "LGAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([LGAppDelegate class]));
+    }
+}
